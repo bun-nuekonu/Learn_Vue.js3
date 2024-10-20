7885652fecbd1fc06b258bf9fe7f33ae4f2c2ee8
bun <bun29268@gmail.com>
2024-10-20 14:52:31 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld :title="message" v-on:result-event="appAction"/>
+    <hr>
+    <p>{{ result }}</p>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: "It's me, Mario!",
+      result: 'no event...',
+    }
+  },
+  methods: {
+    appAction(message) {
+      this.result = '(*** you send: "' + message + '" ***)'
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="alert alert-info">
+    <h2>{{ title }}</h2>
+    <p ref="msg">{{ message }}</p>
+    <div>
+      <button class="btn btn-primary" v-on:click="doAction">Click!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'HelloWorld',
+      message: 'This is sample message.',
+    }
+  },
+  mounted() {
+    this.counter = 0
+  },
+  methods: {
+    doAction() {
+      this.counter++
+      this.$refs.msg.innerHTML += '<h6>counted: ' + String.fromCharCode(64 + this.counter) + '</h6>'
+    }
+  }
+}
+</script>
